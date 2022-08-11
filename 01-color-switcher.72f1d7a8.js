c1b5bb757eb7d20a6b524cefff5a107b0f15db86
TolstikovAlex <100082672+TolstikovAlex@users.noreply.github.com>
2022-08-11 12:51:03 +0000

    Deploying to gh-pages from @ TolstikovAlex/goit-js-hw-09@239ced59c96949da4b8f108bb120394cf65c0d11 🚀

--- 01-color-switcher.72f1d7a8.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");e.disabled=!0,t.addEventListener("click",(function(){timerId=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.72f1d7a8.js.map
